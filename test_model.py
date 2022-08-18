a2f54cf814594219decd99970c753983dd4e10ab
Raul Novelo <rnovelo.cruz98@gmail.com>
2022-08-18 11:52:27 -0500

    Refactor Batch model to support deallocate feature

--- test_model.py
+++ test_model.py
@@ -1,14 +1,18 @@
 from datetime import date, timedelta
+
 import pytest
 
-from model import OrderLine, Batch
+from model import Batch, OrderLine
+
 today = date.today()
 tomorrow = today + timedelta(days=1)
 later = tomorrow + timedelta(days=10)
 
 
 def make_batch_and_line(sku, batch_qty, line_qty):
-    return Batch("batch-001", sku, batch_qty, eta=today), OrderLine("order-123", sku, line_qty)
+    return Batch("batch-001", sku, batch_qty, eta=today), OrderLine(
+        "order-123", sku, line_qty
+    )
 
 
 def test_allocating_to_a_batch_reduces_the_available_quantity():
@@ -41,9 +45,14 @@ def test_cannot_allocate_if_skus_do_not_match():
     assert batch.can_allocate(different_sku_line) is False
 
 
-def test_prefers_warehouse_batches_to_shipments():
-    pytest.fail("todo")
+def test_can_only_deallocate_allocated_lines():
+    batch, unallocated_line = make_batch_and_line("DECORATIVE-TRINKET", 20, 2)
+    batch.deallocate(unallocated_line)
+    assert batch.available_quantity == 20
 
 
-def test_prefers_earlier_batches():
-    pytest.fail("todo")
+def test_allocation_is_idempotent():
+    batch, line = make_batch_and_line("ANGULAR-DESK", 20, 2)
+    batch.allocate(line)
+    batch.allocate(line)
+    assert batch.available_quantity == 18
